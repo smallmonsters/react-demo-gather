5c03a61d6bb344da0ccb8238465cd4464a46f0ff
smallmonsters <13657618017@163.com>
2020-02-13 17:57:58 +0800

    准备重新规划项目

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
-import { BrowserRouter,Route } from 'react-router-dom';
+import { BrowserRouter, Route, Prompt } from 'react-router-dom';
 import routes from './router/index';
 
 const App = () => {
+  function confirma(message:any, callback:any) {
+    console.log(message, callback);
+  }
   return (
     <div className="App">
-      <BrowserRouter>
-      {routes.map(v => <Route key={v.path} exact={v.exact} path={v.path} component={v.component}></Route>)}
+      <BrowserRouter getUserConfirmation={confirma}>
+        <Prompt message="hahahahaha">
+        </Prompt>
+        {routes.map(v => <Route key={v.path} exact={v.exact} path={v.path} component={v.component}></Route>)}
       </BrowserRouter>
     </div>
   );
