fd7e4d1398108772dd8aa7f2c2a5a0d98cfc3e70
smallmonsters <34727387+smallmonsters@users.noreply.github.com>
2020-03-09 18:03:31 +0800

    add useRef

--- src/examples/react/useRef.tsx
+++ src/examples/react/useRef.tsx
@@ -1,11 +1,23 @@
-import React, { Component, useRef } from 'react';
+import React, { Component, useRef, useState, useEffect, useCallback, Fragment } from 'react';
 import { Title } from '@/assets/style';
 
 function RefHook() {
-  const myRef = useRef(<span>22</span>);
-  console.log(myRef.current);
+  const [inputValue, setInputValue] = useState("");
+  const [count, setCount] = useState(0);
+  const changeValue = (e) => {
+    console.log(myRef1);
+    setCount(count + 1)
+    setInputValue(e.target.value)
+  }
+  const myRef = useRef(<input name="custom" value={inputValue} onChange={changeValue} />);
+  const myRef1 = useRef(0);
+  useEffect(() => {
+    myRef1.current = count
+  });
   return (
-    <div>{myRef.current}</div>
+    <Fragment>
+      <div onClick={changeValue}>{myRef.current}{count}</div>
+    </Fragment>
   )
 }
 
@@ -13,19 +25,19 @@ export class UseRef extends Component {
   myRef = React.createRef<HTMLElement>()
   constructor(props) {
     super(props)
-    console.log(this.myRef.current);
+    console.log(this.myRef);
   }
   render() {
     return (
       <div>
         <Title>UseRef</Title>
         <RefHook></RefHook>
-        <span ref={this.myRef} onClick={this.print}>111</span>
+        <span ref={this.myRef} onClick={this.print}>createRef</span>
       </div>
     );
   }
   print = () => {
-    console.log(this.myRef.current);
+    console.log(this.myRef);
   }
 }
 
